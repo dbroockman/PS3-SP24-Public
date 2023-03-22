3480a9f34672af33b016389939115fc9b074c691
David Broockman <dbroockman@users.noreply.github.com>
2023-03-22 14:58:14 -0700

    w10a1g q2 q3 tests

--- w10/w10a1g/student/tests/q2.R
+++ w10/w10a1g/student/tests/q2.R
@@ -7,9 +7,13 @@ test = list(
       points = 1.0,
       code = {
         g.correct.q2 <- qplot(marriage_opinion, marriage_perception, data = democrats.only) + geom_smooth(method = 'lm')
+        if(length(dem.graph.with.line$layers) < 2) {
+            question.correct <- FALSE
+        } else {
         question.correct <- all.equal(g.correct.q2$data, dem.graph.with.line$data) &
                             all.equal(g.correct.q2$mapping, dem.graph.with.line$mapping) &
                             all.equal(g.correct.q2$layers[[2]]$constructor, dem.graph.with.line$layers[[2]]$constructor)
+        }
         testthat::expect_true(question.correct,
                                     info = "dem.graph.with.line does not look right.")
       }
--- w10/w10a1g/student/tests/q3.R
+++ w10/w10a1g/student/tests/q3.R
@@ -17,9 +17,13 @@ test = list(
       points = 0.5,
       code = {
         rep.graph.with.line.correct <- qplot(marriage_opinion, marriage_perception, data = republicans.only) + geom_smooth(method = 'lm')
+        if(length(rep.graph.with.line$layers) < 2) {
+            question.correct <- FALSE
+        } else {
         question.correct <- all.equal(rep.graph.with.line.correct$data, rep.graph.with.line$data) & 
-            all.equal(rep.graph.with.line.correct$mapping, rep.graph.with.line$mapping)
-        question.correct <- question.correct == TRUE
+            all.equal(rep.graph.with.line.correct$mapping, rep.graph.with.line$mapping) &
+            all.equal(g.correct.q2$layers[[2]]$constructor, dem.graph.with.line$layers[[2]]$constructor)
+        }
         testthat::expect_true(question.correct,
                                     info = "rep.graph.with.line does not look right.")
       }
